107cafb6bf0730e79d18982fd3cdca284e20a67c
hakanimamogluspc-debug <hakanimamogluspc@gmail.com>
2026-04-10 15:27:46 +0300

    feat: add Meta Pixel tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -295,6 +295,24 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2156406151837976');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         {children}
@@ -302,3 +320,4 @@ export default function RootLayout({
     </html>
   );
 }
+
