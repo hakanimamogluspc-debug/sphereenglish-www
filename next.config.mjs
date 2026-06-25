ae80d3494ca4df11003d03a3ac68b7f61c0348b4
Hakan Imamoglu <hakanimamogluspc@gmail.com>
2026-06-25 13:57:24 +0300

    fix(payment): CSP'ye wildcard *.iyzipay.com + script-src-elem

--- next.config.mjs
+++ next.config.mjs
@@ -61,11 +61,12 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              // Iyzico checkout script + GA
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.iyzipay.com https://sandbox-merchant.iyzipay.com https://merchant.iyzipay.com https://cpp.iyzipay.com https://sandbox-cpp.iyzipay.com https://static.iyzipay.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://static.iyzipay.com",
+              // Iyzico checkout script (cdn, www, cpp, static, merchant tüm subdomain'ler) + GA
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://*.iyzipay.com",
+              "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://*.iyzipay.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.iyzipay.com",
               "img-src 'self' data: https:",
-              "font-src 'self' data: https://fonts.gstatic.com https://static.iyzipay.com",
+              "font-src 'self' data: https://fonts.gstatic.com https://*.iyzipay.com",
               // Iyzico ile XHR/WebSocket
               "connect-src 'self' https://app.sphereenglish.com https://www.google-analytics.com https://region1.google-analytics.com https://www.iyzipay.com https://*.iyzipay.com",
               // Iyzico 3D Secure ve checkout form iframe'leri
